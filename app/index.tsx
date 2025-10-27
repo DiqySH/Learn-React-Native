a0a76e0c25f161334c5430e3e7a796d34964c327
DiqySH <margolang55@gmail.com>
2025-10-27 10:59:25 +0700

    tugas - 2

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,15 @@
-import { Text, View } from "react-native";
+import Tugas2 from "@/components/Tugas2";
+import { View } from "react-native";
 
 export default function Index() {
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
       }}
     >
-      <Text>Welcome, Diqy 👋</Text>
+      <Tugas2 />
+      {/* <Text>Welcome, Diqy 👋</Text> */}
     </View>
   );
 }
